336e90c33ae4f353a649c6ddb5a70b894ff08ee4
Yuliia <yuliadzyoba@gmail.com>
2025-05-21 16:45:54 +0300

    add Navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navigation.module.css';
+
+const Navigation = () => {
+  return (
+    <div>
+      <h2>Navigation</h2>
+    </div>
+  );
+};
+
+export default Navigation;
