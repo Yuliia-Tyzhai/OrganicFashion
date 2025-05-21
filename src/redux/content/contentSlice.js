88bb66a91eb30a263476be48db8bdbf01f711d26
Yuliia <yuliadzyoba@gmail.com>
2025-05-21 16:32:38 +0300

    add aboutUs to initialState

--- src/redux/content/contentSlice.js
+++ src/redux/content/contentSlice.js
@@ -188,6 +188,32 @@ const initialState = {
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
   },
+
+  aboutUs: {
+    title: 'About Us',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    backgroundImages: {
+      aboutBg: {
+        mobile: {
+          x1: '/src/assets/images/mob/about-us-mob-img-min.png',
+          x2: '/src/assets/images/mob/about-us-mob-img@2x-min.png',
+        },
+        tablet: {
+          x1: '/src/assets/images/tab/about-us-tab-img-min.png',
+          x2: '/src/assets/images/tab/about-us-tab-img@2x-min.png',
+        },
+        desktop: {
+          x1: '/src/assets/images/desk1/about-us-desk1-img-min.png',
+          x2: '/src/assets/images/desk1/about-us-desk1-img@2x-min.png',
+        },
+        largeDesktop: {
+          x1: '/src/assets/images/desk2/about-us-desk2-img-min.png',
+          x2: '/src/assets/images/deak2/about-us-desk2-img@2x-min.png',
+        },
+      },
+    },
+  },
 };
 
 const contentSlice = createSlice({
