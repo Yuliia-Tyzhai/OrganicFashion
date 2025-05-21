e0bc187689b0400e60afbb57f3419eb7ef76f02a
Yuliia <yuliadzyoba@gmail.com>
2025-05-21 16:19:23 +0300

    add shop ti initialState

--- src/redux/content/contentSlice.js
+++ src/redux/content/contentSlice.js
@@ -26,7 +26,115 @@ const initialState = {
       },
     },
   },
+
+  shop: {
+    title: 'Shop by Categories',
+    images: {
+      items: [
+        {
+          mobile: {
+            x1: '/src/assets/images/mob/shop-mob-img-1-min.png',
+            x2: '/src/assets/images/mob/shop-mob-img-1@2x-min.png',
+          },
+          tablet: {
+            x1: '/src/assets/images/tab/shop-tab-img-1-min.png',
+            x2: '/src/assets/images/tab/shop-tab-img-1@2x-min.png',
+          },
+          desktop: {
+            x1: '/src/assets/images/desk1/shop-desk1-img-1-min.png',
+            x2: '/src/assets/images/desk1/shop-desk1-img-1@2x-min.png',
+          },
+          largeDesktop: {
+            x1: '/src/assets/images/desk2/shop-desk2-img-1-min.png',
+            x2: '/src/assets/images/desk2/shop-desk2-img-1@2x-min.png',
+          },
+        },
+        {
+          mobile: {
+            x1: '/src/assets/images/mob/shop-mob-img-2-min.png',
+            x2: '/src/assets/images/mob/shop-mob-img-2@2x-min.png',
+          },
+          tablet: {
+            x1: '/src/assets/images/tab/shop-tab-img-2-min.png',
+            x2: '/src/assets/images/tab/shop-tab-img-2@2x-min.png',
+          },
+          desktop: {
+            x1: '/src/assets/images/desk1/shop-desk1-img-2-min.png',
+            x2: '/src/assets/images/desk1/shop-desk1-img-2@2x-min.png',
+          },
+          largeDesktop: {
+            x1: '/src/assets/images/desk2/shop-desk2-img-2-min.png',
+            x2: '/src/assets/images/desk2/shop-desk2-img-2@2x-min.png',
+          },
+        },
+        {
+          mobile: {
+            x1: '/src/assets/images/mob/shop-mob-img-3-min.png',
+            x2: '/src/assets/images/mob/shop-mob-img-3@2x-min.png',
+          },
+          tablet: {
+            x1: '/src/assets/images/tab/shop-tab-img-3-min.png',
+            x2: '/src/assets/images/tab/shop-tab-img-3@2x-min.png',
+          },
+          desktop: {
+            x1: '/src/assets/images/desk1/shop-desk1-img-3-min.png',
+            x2: '/src/assets/images/desk1/shop-desk1-img-3@2x-min.png',
+          },
+          largeDesktop: {
+            x1: '/src/assets/images/desk2/shop-desk2-img-3-min.png',
+            x2: '/src/assets/images/desk2/shop-desk2-img-3@2x-min.png',
+          },
+        },
+        {
+          mobile: {
+            x1: '/src/assets/images/mob/shop-mob-img-4-min.png',
+            x2: '/src/assets/images/mob/shop-mob-img-4@2x-min.png',
+          },
+          tablet: {
+            x1: '/src/assets/images/tab/shop-tab-img-4-min.png',
+            x2: '/src/assets/images/tab/shop-tab-img-4@2x-min.png',
+          },
+          desktop: {
+            x1: '/src/assets/images/desk1/shop-desk1-img-4-min.png',
+            x2: '/src/assets/images/desk1/shop-desk1-img-4@2x-min.png',
+          },
+          largeDesktop: {
+            x1: '/src/assets/images/desk2/shop-desk2-img-4-min.png',
+            x2: '/src/assets/images/desk2/shop-desk2-img-4@2x-min.png',
+          },
+        },
+        {
+          mobile: {
+            x1: '/src/assets/images/mob/shop-mob-img-5-min.png',
+            x2: '/src/assets/images/mob/shop-mob-img-5@2x-min.png',
+          },
+          tablet: {
+            x1: '/src/assets/images/tab/shop-tab-img-5-min.png',
+            x2: '/src/assets/images/tab/shop-tab-img-5@2x-min.png',
+          },
+          desktop: {
+            x1: '/src/assets/images/desk1/shop-desk1-img-5-min.png',
+            x2: '/src/assets/images/desk1/shop-desk1-img-5@2x-min.png',
+          },
+          largeDesktop: {
+            x1: '/src/assets/images/desk2/shop-desk2-img-5-min.png',
+            x2: '/src/assets/images/desk2/shop-desk2-img-5@2x-min.png',
+          },
+        },
+      ],
+    },
+  },
 };
+
+//     newCollection: {
+//         title: 'New Collection',
+//         images: {
+//             items: [
+
+//             ],
+//         },
+//         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+// },
 
 const contentSlice = createSlice({
   name: 'content',
