98cbb2ff5de8dc0cc40fec04bb834566f4cf632e
Yuliia <yuliadzyoba@gmail.com>
2025-05-22 14:47:48 +0300

    add NewCollection

--- src/pages/NewCollection/NewCollection.jsx
+++ src/pages/NewCollection/NewCollection.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
-import './NewCollection.module.css';
+import { useSelector } from 'react-redux';
+import { selectNewCollection } from '../../redux/content/selectors';
+import styles from './NewCollection.module.css';
 
 const NewCollection = () => {
+  const newCollectionData = useSelector(selectNewCollection);
+
   return (
-    <div>
-      <h2>NewCollection</h2>
-    </div>
+    <section className={styles.newCollection}>
+      <h2 className={styles.title}>{newCollectionData.title}</h2>
+
+      <div className={styles.imageGrid}>
+        {newCollectionData.images.items.map((image, index) => (
+          <div key={index} className={styles.imageContainer}>
+            <picture>
+              <source
+                srcSet={`${image.largeDesktop.x1} 1x, ${image.largeDesktop.x2} 2x`}
+                media="(min-width: 1920px)"
+              />
+              <source
+                srcSet={`${image.desktop.x1} 1x, ${image.desktop.x2} 2x`}
+                media="(min-width: 1366px)"
+              />
+              <source
+                srcSet={`${image.tablet.x1} 1x, ${image.tablet.x2} 2x`}
+                media="(min-width: 992px)"
+              />
+              <source
+                srcSet={`${image.mobile.x1} 1x, ${image.mobile.x2} 2x`}
+                media="(max-width: 991px)"
+              />
+              <img
+                src={image.mobile.x1}
+                alt={`New Collection ${index + 1}`}
+                className={styles.image}
+              />
+            </picture>
+          </div>
+        ))}
+      </div>
+      <p className={styles.description}>{newCollectionData.description}</p>
+    </section>
   );
 };
 
